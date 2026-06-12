7246a5cc8dbc086d7821c1f61386ed75d949893a
eksdee-sama <dsampatalwar@gmail.com>
2026-06-12 20:56:16 +0530

    Force push local changes (Postman collection & env)

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import PlaceSearchView, RouteMapView, RouteView
-
-urlpatterns = [
-    path("route/", RouteView.as_view(), name="route"),
-    path("route/map/", RouteMapView.as_view(), name="route-map"),
-    path("search/", PlaceSearchView.as_view(), name="place-search"),
-]
